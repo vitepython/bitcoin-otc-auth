773a2c28aefe0815fc6c64868dde7167cb80b329
Pensando Panama <vitepython@users.noreply.github.com>
2025-02-27 01:28:38 -0500

    Update otcauth.py
    Key Changes:
    Shebang Line: Updated to #!/usr/bin/env python3 to ensure the script uses Python 3.
    
    Imports:
    
    Replaced httplib with http.client.
    
    Updated Tkinter to tkinter.
    
    Removed unused sys and os imports.
    
    Print Statements: Converted to the print() function.
    
    Raw Input: Replaced raw_input with input for Python 3.
    
    HTTP Response: Decoded the response to ensure compatibility with Python 3.

--- otcauth.py
+++ otcauth.py
@@ -1,41 +1,40 @@
-#!/usr/bin/env python
-import httplib
+#!/usr/bin/env python3
+
+import http.client
 import gnupg
 import getpass
-from Tkinter import *
+import tkinter as tk
 
-secret = getpass.getpass("enter password")
-path = getpass.getpass("~/path/to/youre/key/") # alternative is hardcoding or using arguments
+secret = getpass.getpass("Enter password: ")
+path = getpass.getpass("Enter the path to your key (default is ~/path/to/your/key/): ")
 
 def auth():
+    # Retrieve the encrypted message
+    conn = http.client.HTTPConnection("bitcoin-otc.com")
+    conn.request("GET", "/otps/KEY-FINGER-PRINT")
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
 
-# Retrieve the encrypted message
-        conn = httplib.HTTPConnection("bitcoin-otc.com")
-        conn.request("GET", "/otps/KEY-FINGER-PRINT")
-        r1 = conn.getresponse()
-        print r1.status, r1.reason
+    # One time OTC secret phrase
+    otc = r1.read().decode()
+    conn.close()
+    print(otc)
 
-        # one time OTC secretphrase
-        otc = r1.read()
-        conn.close()
-        print otc
+    # Decrypt the message
+    gpg = gnupg.GPG(gnupghome=path)
+    decrypted_data = gpg.decrypt(otc, passphrase=secret)
+    print(decrypted_data)
 
-        # decrypt the message
-        gpg = gnupg.GPG(gnupghome=path)
-        decrypted_data = gpg.decrypt(otc, passphrase=secret)
-        print decrypted_data
+    # Open a window with the decrypted message
+    root = tk.Tk()
+    w = tk.Label(root, text=str(decrypted_data))
+    w.pack()
 
-        # open a window with the decrypted message
-        master = Tk()
-        w = Label(master, text = decrypted_data)
-        w.pack()
+    # Copy decrypted message to clipboard
+    root.withdraw()
+    root.clipboard_clear()
+    root.clipboard_append(str(decrypted_data))
+    root.destroy()
+    root.mainloop()
 
-        # copy decrypted message to clipboard
-        r = Tk()
-        r.withdraw()
-        r.clipboard_clear()
-        r.clipboard_append(decrypted_data)
-        r.destroy()
-        mainloop()
-        
 auth()
